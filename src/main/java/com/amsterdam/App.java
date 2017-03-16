e698d36a4aff48498409fda2fade601e3824d36c
AlexandraBrasoveanu <alexandra_br@ymail.com>
2017-03-16 01:02:13 +0100

    Added a new example input image, added new logic to support some of the edge cases

--- src/main/java/com/amsterdam/App.java
+++ src/main/java/com/amsterdam/App.java
@@ -7,45 +7,38 @@ import com.amsterdam.services.FileProcessor;
 import com.amsterdam.services.ObjectFinder;
 import com.amsterdam.services.imageprocessing.ImageFileProcessor;
 import com.amsterdam.spaceInvaders.SpaceInvaderFinder;
+import com.util.MatrixFormatter;
 import com.util.Point;
 
 import java.io.File;
 import java.text.MessageFormat;
+import java.util.Map;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
-     private static void displayMatrix(int[][] newMatrix) {
-        for(int i=0; i<newMatrix.length; i++) {
-            for(int j=0; j < newMatrix[0].length; j++) {
-                    System.out.print(newMatrix[i][j]) ;
-            }
-                System.out.println();
-        }
-    }
-
-    private static void displayMatrix(char[][] newMatrix) {
-        for(int i=0; i<newMatrix.length; i++) {
-            for(int j=0; j<newMatrix[0].length; j++) {
-                    System.out.print(newMatrix[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
     public static void main( String[] args )
     {
         Injector injector = Guice.createInjector(new ObjectPatternConfiguration());
         ObjectFinder imageObjectFinder = injector.getInstance(SpaceInvaderFinder.class);
-        File inFile = new File("C:\\radarPage.txt");
+        MatrixFormatter formatter = injector.getInstance(MatrixFormatter.class);
+
+        //read and process input file containing the radar image
+        File inFile = new File("radarPage.txt");
         FileProcessor processor  = injector.getInstance(ImageFileProcessor.class);
-        int[][] oldMatrix = processor.fileTo2D(inFile);
-        for(ImageElement croppedImage : imageObjectFinder.find(new Image(oldMatrix)).keySet()) {
+        Image imageMatrix = new Image(processor.fileTo2D(inFile));
+
+        // find a list of the best matching image elements and the corresponding originals
+        Map<ImageElement, ImageElement> detectionMapping = imageObjectFinder.find(imageMatrix);
+
+        // pretty the location of the found match in the original image
+        // and both the image element and the original pattern for reference
+        for(ImageElement croppedImage : detectionMapping.keySet()) {
             Point startingPct = croppedImage.getStartingPoint();
             System.out.println(MessageFormat.format("Found space invader at point ({0},{1})", startingPct.getX(), startingPct.getY()));
+            System.out.println("Original pattern: ");
+            System.out.println(formatter.prettyFormat(detectionMapping.get(croppedImage).data));
+            System.out.println("Matching pattern: ");
+            System.out.println(formatter.prettyFormat(croppedImage.data));
         }
     }
 }
